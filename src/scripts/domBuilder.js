f7ec0a377f73c022dd70588702ba427f3ae9a5bc
dakotacooksongit config --global user.namegit config --global user.name dakotacookson <dakota.cookson1995@gmail.com>
2018-12-10 14:14:44 -0600

    built dom builder with deku sheild

---
+++ src/scripts/domBuilder.js
@@ -0,0 +1,62 @@
+
+let ParkChoiceCreate = document.createElement("Section");
+let ParkChoiceCreateButton = document.createElement("Button");
+let ParkChoiceClassAdd = ParkChoiceCreate.classList.add("ParkResultTemp");
+let ParkChoiceClassGet = document.querySelector(".results__Container")
+ParkChoiceClassGet.appendChild(ParkChoiceCreateButton);
+ParkChoiceClassGet.appendChild(ParkChoiceCreate, "hi park");
+console.log(ParkChoiceCreate);;
+
+
+let FoodChoiceCreate = document.createElement("Section");
+let FoodChoiceCreateButton = document.createElement("Button");
+let FoodChoiceClassAdd = FoodChoiceCreate.classList.add("FoodResultTemp");
+let FoodChoiceClassGet = document.querySelector(".results__Container")
+FoodChoiceClassGet.appendChild(FoodChoiceCreateButton);
+FoodChoiceClassGet.appendChild(FoodChoiceCreate, "hi food");
+console.log(FoodChoiceCreate);;
+
+
+let MeetupChoiceCreate = document.createElement("Section");
+let MeetupChoiceCreateButton = document.createElement("Button");
+let MeetupChoiceClassAdd = MeetupChoiceCreate.classList.add("MeetupResultTemp");
+let MeetupChoiceClassGet = document.querySelector(".results__Container")
+MeetupChoiceClassGet.appendChild(MeetupChoiceCreateButton);
+MeetupChoiceClassGet.appendChild(MeetupChoiceCreate, "hi Meetup");
+console.log(MeetupChoiceCreate);;
+
+
+let ConcertsChoiceCreate = document.createElement("Section");
+let ConcertsChoiceCreateButton = document.createElement("Button");
+let ConcertsChoiceClassAdd = ConcertsChoiceCreate.classList.add("ConcertsResultTemp");
+let ConcertsChoiceClassGet = document.querySelector(".results__Container")
+ConcertsChoiceClassGet.appendChild(ConcertsChoiceCreateButton);
+ConcertsChoiceClassGet.appendChild(ConcertsChoiceCreate, "hi  Concerts");
+console.log(ConcertsChoiceCreate);;
+
+let ParkChoiceCreateSaved = document.createElement("Section");
+let ParkChoiceClassSavedAdd = ParkChoiceCreateSaved.classList.add("ParkResultSaved");
+let ParkChoiceClassSavedGet = document.querySelector(".itinerary__Container")
+ParkChoiceClassSavedGet.appendChild(ParkChoiceCreateSaved, "hi park Saved");
+console.log(ParkChoiceCreateSaved);;
+
+
+let FoodChoiceCreateSaved = document.createElement("Section");
+let FoodChoiceClassSavedAdd = FoodChoiceCreateSaved.classList.add("FoodResultSaved");
+let FoodChoiceClassSavedGet = document.querySelector(".itinerary__Container")
+FoodChoiceClassSavedGet.appendChild(FoodChoiceCreateSaved, "hi Food Saved");
+console.log(FoodChoiceCreateSaved);;
+
+
+let MeetupChoiceCreateSaved = document.createElement("Section");
+let MeetupChoiceClassSavedAdd = MeetupChoiceCreateSaved.classList.add("MeetupResultSaved");
+let MeetupChoiceClassSavedGet = document.querySelector(".itinerary__Container")
+MeetupChoiceClassSavedGet.appendChild(MeetupChoiceCreateSaved, "hi Meetup Saved");
+console.log(MeetupChoiceCreateSaved);;
+
+
+let ConcertsChoiceCreateSaved = document.createElement("Section");
+let ConcertsChoiceClassSavedAdd = ConcertsChoiceCreateSaved.classList.add("ConcertsResultSaved");
+let ConcertshoiceClassSavedGet = document.querySelector(".itinerary__Container")
+ConcertshoiceClassSavedGet.appendChild(ConcertsChoiceCreateSaved, "hi Concerts Saved");
+console.log(ConcertsChoiceCreateSaved);;
