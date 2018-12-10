aed75e821a595b7f9c26d5274a6aa019f5bec85b
JD <jdwheeler0@gmail.com>
2018-12-10 09:37:07 -0600

    added api for parks did some HTML and CSS framework

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,15 +1,19 @@
-const parkData = {
-    getResources() {
-      return fetch("https://data.nashville.gov/resource/xbru-cfzi.json")
-      .then(function(response){
-        response.json()
-    })
-    .then(response => console.log(response));
-    },
-    queryResources(userInput) {
-      return fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?q=${userInput}`)
-      .then(response => response.json());
-    }
-  };
 
-  parkData.getResources();
+// This is JD's code for pulling data on Parks.
+
+let parkDataContainer = {};
+
+//const parkData = {
+   // getResources() {
+      fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=lfAtpDX8XuYvee5T9U6FnVpFP",
+      {Authorization: {Bearer: "lfAtpDX8XuYvee5T9U6FnVpFP"}})
+      .then(parkDataContainer => parkDataContainer.json())
+      .then(parkDataContainer => {
+         console.log(parkDataContainer);
+      });
+      //}
+   
+  // }
+
+
+  console.log(parkDataContainer);
