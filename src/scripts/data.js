bca8303b90e6808adf80ce0238fa97c8aba0822b
Nick Hansen <nick.hansen13@gmail.com>
2018-12-10 09:10:10 -0600

    changed code to get Nick api working

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,24 +1,19 @@
-/* Zomato API - Nick
-user-key: "b370c0e063c48a8441ba08df6238f015"
-city id: 1138 */
+// Zomato API - Nick
 
-fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&count=10&radius=25")
-    .then(search => search.json())
-    .then(parsedSearch => {
-        console.table(parsedSearch)
+let foodData = {};
+fetch("https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&count=10&radius=25",
+   {headers: {
+        "user-key": "b370c0e063c48a8441ba08df6238f015"
+}})
+    .then(foodData => foodData.json())
+    .then(foodData => {
+        console.table(foodData);
     });
-const parkData = {
-    getResources() {
-      return fetch("https://data.nashville.gov/resource/xbru-cfzi.json")
-      .then(function(response){
-        response.json()
-    })
-    .then(response => console.log(response));
-    },
-    queryResources(userInput) {
-      return fetch(`https://data.nashville.gov/resource/xbru-cfzi.json?q=${userInput}`)
-      .then(response => response.json());
-    }
-  };
 
-  parkData.getResources();
+    
+let parkData = {};
+fetch("https://data.nashville.gov/resource/xbru-cfzi.json?$$app_token=lfAtpDX8XuYvee5T9U6FnVpFP",
+    {Authorization: {Bearer: "lfAtpDX8XuYvee5T9U6FnVpFP"}})
+    .then(parkData => parkData.json())
+    .then(parkData => {
+        console.log(parkData)})
