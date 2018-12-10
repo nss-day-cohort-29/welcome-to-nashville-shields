63db7b74393b8ebca4c96e78af809e92b2a862b2
dek <deeeqhaji@gmail.com>
2018-12-10 09:07:14 -0600

    some update

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,10 +1,9 @@
-const data = {
-    getResources() {
-      return fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=55BlEfJmY55sowbvAYAaU7P5KzxOGaOL")
-      .then(function(response){
-        response.json();
-      })
-      .then(response => response.json());
-    }
-  };
-  console.log(getResources)
+let concertData = {};
+fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=55BlEfJmY55sowbvAYAaU7P5KzxOGaOL",
+   {Authorization: {Bearer: "55BlEfJmY55sowbvAYAaU7P5KzxOGaOL"}})
+   .then(concertData => concertData.json())
+   .then(concertData => {
+       console.table(concertData)})
+
+
+
