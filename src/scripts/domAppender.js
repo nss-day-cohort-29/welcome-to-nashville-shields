ba721b1a726b7487d75c0313bc2f9961fe33fefc
JD <jdwheeler0@gmail.com>
2018-12-11 10:00:21 -0600

    Big Changes to domAppender.js and Data.js

---
+++ src/scripts/domAppender.js
@@ -0,0 +1,11 @@
+// In the appender we are going to accomplish...
+//  Take elements from HTML/ domBuilder that are based on user interation and append them to contatiners that are appear on HTML.
+//  1) Call user search input elements container from DomBuilder.
+//  2) Append input to search results container. 
+//  3) Collect user selects/chooses/saves and creating element for that choice. 
+//  3.5) Call the results selection. 
+//  4) Append results selection to coresponding intinerary element. 
+//  5) Drink Bueno BEER none of that cheap ass rot gut!!
+
+
+
